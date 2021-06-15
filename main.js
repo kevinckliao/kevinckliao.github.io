87ea97647384bb8c4f9a570b5cb2221cc9b2b20a
KevinLiao <kevin.ck.liao@gmail.com>
2021-06-15 14:57:36 +0800

    add Flashing Marker

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var dateStart = new Date("2021-02-21T00:00:00.000+08:00"); //reguest a UTC+8 tim
 var msecStart = dateStart.getTime();
 var msecNow = Date.now();  // get msec of Local-Time
 var weekNo =  Math.floor((msecNow - msecStart) / ( 7* 24 * 3600000)) + 1;
+var weekType = 0;
 
 // get Today's Date & Week String
 const dateNow = new Date();
@@ -13,12 +14,30 @@ const weekdayStr = [ '日','ㄧ','二','三','四','五','六' ];
 
 // assembly display string
 var msgWeek = '今天是';
-if ((weekNo % 2) == 0) {  msgWeek += " 雙周"; } 
-else { msgWeek += " 單周"; };
+if ((weekNo % 2) == 0) {  msgWeek += " 雙周"; weekType = 1;} 
+else { msgWeek += " 單周"; weekType = 0;};
 
-msgWeek += ' 星期' + weekdayStr[dateNow.getDay()] ;
-msgWeek += ' (' + dateNow.toLocaleDateString();
-msgWeek += ' - 第' + weekNo + '周)' ;
+msgWeek += ' / 星期' + weekdayStr[dateNow.getDay()] ;
+msgWeek += '  &nbsp; (' + dateNow.toLocaleDateString();
+msgWeek += ' 第' + weekNo + '周)' ;
 
-console.log(msgWeek);
+//console.log(msgWeek);
 document.getElementById("weekmsg").innerHTML = msgWeek ;
+
+//
+// mark Day
+//
+function markDay (weekType, dayNum) {
+  var tableEl = document.getElementsByTagName('table') ;
+  //console.log(tableEl);
+
+  cellEl = tableEl[weekType].getElementsByTagName('th');
+
+  //cellEl[ dayNum ].style.backgroundColor = '#fa520f' ;
+  //cellEl[ dayNum ].style.color = '#ffffff' ;
+  cellEl[ dayNum ].classList.add('marker-flash');
+  //console.log('even/odd: ' + weekType + ' day: ' + dayNum);
+}
+
+//mark today
+markDay(weekType, dateNow.getDay());
